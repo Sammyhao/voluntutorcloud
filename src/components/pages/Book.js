03f1851af5cd33dc7b416b6f0864be368149a95a
胤翔 郝 <samyhao0507@gmail.com>
2022-04-26 13:37:03 +0800

    whatever

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -2,14 +2,26 @@ import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
+import S_Book from '../S_Booking'
 import Book from '../Booking'
 
 export default function Appointment() {
+  let role = 1;
+  if(role==0){
   return (
     <>
       <Navbar></Navbar>
       <Book></Book>
       <Footer></Footer>
     </>
-  )
+  )}else{
+    return(
+      <>
+      <Navbar></Navbar>
+      <S_Book></S_Book>
+      <Footer></Footer>
+
+      </>
+    )
+  }
 }
