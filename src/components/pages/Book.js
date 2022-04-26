f6ba4be44b91fe33a60582533573a5fa77409a05
胤翔 郝 <samyhao0507@gmail.com>
2022-04-26 13:41:00 +0800

    whatever

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
-import S_Book from '../S_Booking'
+import S_Booking from '../S_Booking'
 import Book from '../Booking'
 
 export default function Appointment() {
@@ -18,7 +18,7 @@ export default function Appointment() {
     return(
       <>
       <Navbar></Navbar>
-      <S_Book></S_Book>
+      <S_Booking></S_Booking>
       <Footer></Footer>
 
       </>
