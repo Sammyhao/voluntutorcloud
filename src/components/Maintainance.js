108796c54ae18772e99e256c2a959a66d7338bf8
胤翔 郝 <samyhao0507@gmail.com>
2022-04-28 11:21:13 +0800

    whatever

--- src/components/Maintainance.js
+++ src/components/Maintainance.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Maintainance.css'
 export default function Maintainance() {
     return(
-    <div className = "maintainence">
+    <div className = "maintainance">
         We are currently in maintainance. Sorry for your inconvenience.
     </div>)
 }
