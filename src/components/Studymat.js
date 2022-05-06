665c1bc14fc48c82103dd65934ea583590b4669e
胤翔 郝 <samyhao0507@gmail.com>
2022-05-06 22:19:34 +0800

    hope it works, the links in subject material page

--- src/components/Studymat.js
+++ src/components/Studymat.js
@@ -157,6 +157,13 @@ export default function Studymat() {
             <div id="studydialog">Detailed Materials</div>
             <div id="links">{}</div>
             <div id="links">links</div>
+            {subMat.map((mat) => {
+              return (
+                <>
+                  <div id="links">{mat.chapterDesc}: {mat.link}</div>
+                </>
+              )
+            })}
 
             <div></div>
           </BootstrapDialog>
