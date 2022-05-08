d6b02f70326483a332d60e3948c01004e7ead1cf
胤翔 郝 <samyhao0507@gmail.com>
2022-05-08 11:32:17 +0800

    fix meet link

--- src/components/Appointmentmeet.js
+++ src/components/Appointmentmeet.js
@@ -122,9 +122,9 @@ function Appointmentmeet() {
   
   const meet = () => {
     console.log("google meet link is ", googleMeetLink);
-    console.log("google meet link alt is ", googleMeetLinkalt);
+    // console.log("google meet link alt is ", googleMeetLinkalt);
     if(googleMeetLink) window.location.replace(googleMeetLink);
-    else window.location.replace(googlemeetlinkalt);
+    // else window.location.replace(googlemeetlinkalt);
   }
 
   function updateRecord() {
