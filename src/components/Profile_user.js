fc45bc683bb69b21030f22167f607b336b911cb5
胤翔 郝 <samyhao0507@gmail.com>
2022-05-10 22:52:35 +0800

    sdfsdfsdf

--- src/components/Profile_user.js
+++ src/components/Profile_user.js
@@ -408,20 +408,18 @@ function Profile_user() {
             value={googlemeetlink}
             onChange={(e) => {
               setGooglemeetlink(e.target.value)
-              Axios.post(
-                'https://voluntutorcloud-server.herokuapp.com/updateGooglemeetlink',
-                {
-                  username: name,
-                  googlemeetlink: e.target.value,
-                },
-              ).then((response) => {
-                console.log(response)
-              })
-              setphone(e.target.value)
             }}
           />
           <div className="btn-save" onClick={(e) => {
-
+            Axios.post(
+              'https://voluntutorcloud-server.herokuapp.com/updateGooglemeetlink',
+              {
+                username: name,
+                googlemeetlink: e.target.value,
+              },
+            ).then((response) => {
+              console.log(response)
+            })
           }}>{lm[status]}</div>
         </div>
         <div className="currentprogram">
