7ff568a4caa50a0e4679fb21ed031657649478c0
胤翔 郝 <samyhao0507@gmail.com>
2022-05-14 16:28:03 +0800

    make it better

--- src/components/Msg.js
+++ src/components/Msg.js
@@ -27,6 +27,7 @@ function Msg() {
   // T:asdfasfasdfψS:Let's book a meetψT:omg hi long time no seeψT:HiψT:Sure!!!ψT:See you then!ψT:Sure!!ψS:I am okay with the timeψS:Yes, can we have a meeting then?ψT:Are you available next Tuesday?
 
   function processMsg(msgStr, username, studentname) {
+    setMsgRec([]);
     if(msgStr == "") {
       setLastestMsg("");
     }
