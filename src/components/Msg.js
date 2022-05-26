3cd1fd2a37cdb6760c5f5570f4431bb83d7174d5
Sammyhao <samyhao0507@gmail.com>
2022-05-26 15:01:47 +0800

    Update Msg.js

--- src/components/Msg.js
+++ src/components/Msg.js
@@ -17,10 +17,12 @@ function Msg() {
     teacherusername = ''
   const [curMsg, setCurMsg] = useState('')
   const [msgRec, setMsgRec] = useState([])
-  let msgRecRev = []
+  const [otherMsgRec, setOtherMsgRec] = useState([]);
+  const [tmpMsgRec, setTmpMsgRec] = useState([]);
   let msgStr = ''
   const [msgForUpd, setMsgForUpd] = useState('')
   const [isLoading, setLoading] = useState(true)
+  const [isLoading2, setLoading2] = useState(true)
   const [hasProcessMsg, setHasProcessMsg] = useState(false)
   const [usernameConst, setUsernameConst] = useState('')
   const [studentnameConst, setStudentnameConst] = useState('')
@@ -30,21 +32,17 @@ function Msg() {
 
   // T:asdfasfasdfψS:Let's book a meetψT:omg hi long time no seeψT:HiψT:Sure!!!ψT:See you then!ψT:Sure!!ψS:I am okay with the timeψS:Yes, can we have a meeting then?ψT:Are you available next Tuesday?
 
-  function processMsg(msgStr, username, studentname) {
-    setMsgRec([])
+  function processMsg(curRecep, msgStr, username, studentname) {
     setLastestMsg('')
     console.log(msgStr)
     if (msgStr != '') {
       const msgInfo = msgStr.split('ψ')
       console.log('msgInfo')
       console.log(msgInfo)
-      console.log(msgInfo, msgRec);
+      console.log(msgInfo.length, msgRec.length);
+      if (msgRec.length != msgInfo.length) {
         console.log('has entered msgRec construction condition')
-        for (let i = -1; i < msgInfo.length - 1; i++) {
-          if(i == -1) {
-            setMsgRec([]);
-            continue;
-          }
+        for (let i = 0; i < msgInfo.length - 1; i++) {
           const category = msgInfo[i].split('|')
           if (i == 0) setLastestMsg(category[1])
           let t = ''
@@ -52,18 +50,23 @@ function Msg() {
           if (category[1] == '') continue
           let msg = { type: t, text: category[1] }
           console.log(msg)
-          setMsgRec((msgRec) => [msg, ...msgRec])
+          setTmpMsgRec((msgRec) => [msg, ...msgRec])
         }
-        setMsgRec(msgRec.slice(0, msgInfo.length - 1))
+      } else setTmpMsgRec(msgRec.slice(0, msgInfo.length - 1))
     } else {
-      setMsgRec([])
       setLastestMsg('')
+      setTmpMsgRec([]);
     }
+
+    // assign msg data back
+    if(curRecep == "this") setMsgRec(tmpMsgRec);
+    else setOtherMsgRec(tmpMsgRec);
+    setTmpMsgRec([]);
+
     console.log(teacherusername, studentname)
     setUsernameConst(username)
     setStudentnameConst(studentname)
     setMsgForUpd(msgStr)
-    setHasProcessMsg(true)
   }
 
   const updateMsg = () => {
@@ -139,10 +142,26 @@ function Msg() {
               ).then((response) => {
                 if (response.data.length) msgStr = response.data[0].msg
                 console.log(msgStr)
-                processMsg(msgStr, username, studentname)
+                processMsg("this", msgStr, username, studentname)
                 console.log(msgRec)
                 setLoading(false)
               })
+
+              Axios.post(
+                'https://voluntutorcloud-server.herokuapp.com/getMsg',
+                {
+                  username: username,
+                  studentname: studentname,
+                },
+              ).then((response) => {
+                if (response.data.length) msgStr = response.data[0].msg
+                console.log(msgStr)
+                processMsg("other", msgStr, username, studentname)
+                console.log(msgRec)
+                setLoading2(false)
+              })
+
+              setHasProcessMsg(true)
             }
           })
         },
@@ -211,22 +230,16 @@ function Msg() {
     }
     setStudentnameConst(e)
 
-    console.log(tempstudentname)
-    setMsgRec([]);
+    console.log(tempstudentname, " is the student chosen")
 
-    Axios.post('https://voluntutorcloud-server.herokuapp.com/getMsg', {
-      username: usernameConst,
-      studentname: tempstudentname,
-    }).then((response) => {
-      if (response.data.length) msgStr = response.data[0].msg
-      console.log(msgStr)
-      console.log(msgRec);
-      setMsgRec([]);
-      processMsg(msgStr, usernameConst, tempstudentname);
-    })
+    console.log(otherMsgRec[0].text);
+    setLastestMsg(otherMsgRec[0].text);
+    const tmpMsgRec = msgRec;
+    setMsgRec(otherMsgRec);
+    setOtherMsgRec(tmpMsgRec);
   }
 
-  if (isLoading) {
+  if (isLoading || isLoading2) {
     return <Loading />
   } else {
     console.log('msgRec')
