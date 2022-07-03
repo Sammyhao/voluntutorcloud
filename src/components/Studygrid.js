168a6ff2bd7ff7a8fb0d68af0004f0b7d5687d3e
Ruby Chang <ruby931109@gmail.com>
2022-07-03 10:48:25 +0800

    Update Studygrid.js

--- src/components/Studygrid.js
+++ src/components/Studygrid.js
@@ -25,13 +25,15 @@ export const Studygrid = ({ studymt, status }) => {
             {b[status]}
           </div>
           <div className="schoolstudy">
-            {schoollist.map((e, index) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} lg={4}>
-                  <div className="schoollist">{e}</div>
-                </Grid>
-              )
-            })}
+            <Grid container spacing={4}>
+              {schoollist.map((e, index) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <div className="schoollist">{e}</div>
+                  </Grid>
+                )
+              })}
+            </Grid>
           </div>
           <div className="tagswrap">
             <div className="tagsstudy">{studymt.grade}</div>
