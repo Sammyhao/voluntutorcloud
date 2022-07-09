820f6b33a1cc19119730ccf9bae5c02d6b56aef1
Ruby Chang <ruby931109@gmail.com>
2022-07-09 12:13:06 +0800

    adm

---
+++ src/components/Admin_appointment.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import './Admin_appointment.css'
+export default function Admin_appointment() {
+  return <div>Admin_appointment</div>
+}
---
+++ src/components/pages/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../App.css'
+import Admin_appointment from '../Admin_appointment'
+
+export default function SignIn() {
+  return (
+    <>
+      <Admin_appointment></Admin_appointment>
+    </>
+  )
+}
