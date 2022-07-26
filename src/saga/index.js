8797e561401e4e558f02ea6d934f3ec50e49fa2b
Ruby Chang <ruby931109@gmail.com>
2022-07-26 12:22:58 +0800

    Update index.js

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,18 +1,12 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
-import Axios from 'axios'
 import { login } from '../reducer/user'
-
 const LOGIN = 'user/login'
 const LOGOUT = 'user/logout'
 
 function* fetchData() {
-  const data = yield call(() => {
-    Axios.get('https://voluntutorcloud-server.herokuapp.com/login').then(
-      (response) => {
-        response.data
-      },
-    )
-  })
+  const data = yield call(() =>
+    fetch('https://voluntutorcloud-server.herokuapp.com/login'),
+  )
   console.log('data', data)
   const action = yield data.json()
   console.log('action json', action)
