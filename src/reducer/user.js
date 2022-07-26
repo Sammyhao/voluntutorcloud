ae0f30835da883e1c0f0da58163b762042538b3f
Ruby Chang <ruby931109@gmail.com>
2022-07-26 14:00:26 +0800

    d

--- src/reducer/user.js
+++ src/reducer/user.js
@@ -1,16 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// const Initialstate = { language: '', role: '', login: false }
+// const Initialstate = { language: 1, role: true, login: false }
+const Initialstate = []
+const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS'
+const FETCH_DATA = 'FETCH_DATA'
 
 export const userSlice = createSlice({
   name: 'user',
-  initialState: { value: [] },
+  initialState: { value: Initialstate },
   reducers: {
     login: (state, action) => {
-      state.value = action.payload
+      switch (action.type) {
+        case FETCH_DATA_SUCCESS:
+          state.value = action.payload
+        default:
+          state.value = Initialstate
+      }
     },
     logout: (state) => {
-      // state.value = Initialstate
+      state.value = Initialstate
     },
   },
 })
--- src/saga/index.js
+++ src/saga/index.js
@@ -2,8 +2,8 @@ import { call, put, takeEvery } from 'redux-saga/effects'
 import { login } from '../reducer/user'
 import axios from 'axios'
 
-const LOGIN = 'user/login'
-const LOGOUT = 'user/logout'
+const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS'
+const FETCH_DATA = 'FETCH_DATA'
 
 async function getProcesses() {
   return axios.get(`https://voluntutorcloud-server.herokuapp.com/login`)
@@ -14,11 +14,11 @@ function* fetchData() {
   console.log('data', data)
   const action = yield data.json()
   console.log('action json', action)
-  yield put(login(action))
+  yield put({ type: FETCH_DATA_SUCCESS, payload: { action } })
 }
 
 function* mySaga() {
-  yield takeEvery(LOGIN, fetchData)
+  yield takeEvery(FETCH_DATA, fetchData)
 }
 
 export default mySaga
