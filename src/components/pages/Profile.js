28980ed18a6b80540bf7f58f259b8446aefeb1de
Ruby Chang <ruby931109@gmail.com>
2022-07-29 19:49:40 +0800

    Update Profile.js

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -9,28 +9,36 @@ import Loading from '../Loading'
 import S_Navbar from '../S_Navbar'
 
 export default function Profile() {
-  const [role, setRole] = useState("");
-  const [lang, setLang] = useState("");
-  const [isLoading, setLoading] = useState(true);
-  const [profile, setProfile] = useState({});
-  const [portfolio, setPortfolio] = useState({});
+  const [role, setRole] = useState('')
+  const [lang, setLang] = useState('')
+  const [isLoading, setLoading] = useState(true)
+  const [profile, setProfile] = useState({})
+  const [portfolio, setPortfolio] = useState({})
 
   useEffect(() => {
-
-    if(isLoading) {
-    Axios.get('https://voluntutorcloud-server.herokuapp.com/login').then(
-      (response) => {
-          console.log(response.data.user[0]);
-          setRole(response.data.user[0].role);
-          setLang(response.data.user[0].lang);
-          setProfile(response.data.user[0]);
-          console.log(response.data.user[0].lastname + response.data.user[0].firstname);
-
-          return Axios.post('https://voluntutorcloud-server.herokuapp.com/getProfolio', {name: response.data.user[0].lastname + response.data.user[0].firstname})
-      }).then((response) => {
-        setPortfolio(response.data[0]);
-        setLoading(false);
-      })
+    if (isLoading) {
+      Axios.get('https://voluntutorcloud-server.herokuapp.com/login')
+        .then((response) => {
+          console.log(response.data.user[0])
+          setRole(response.data.user[0].role)
+          setLang(response.data.user[0].lang)
+          setProfile(response.data.user[0])
+          console.log(
+            response.data.user[0].lastname + response.data.user[0].firstname,
+          )
+          return Axios.post(
+            'https://voluntutorcloud-server.herokuapp.com/getProfolio',
+            {
+              name:
+                response.data.user[0].lastname +
+                response.data.user[0].firstname,
+            },
+          )
+        })
+        .then((response) => {
+          setPortfolio(response.data[0])
+          setLoading(false)
+        })
     }
   })
 
@@ -42,7 +50,7 @@ export default function Profile() {
     )
   } else {
     if (role == 'teacher') {
-      console.log(lang, profile);
+      console.log(lang, profile)
       return (
         <>
           <Navbar lang={lang} isLoggedIn={true}></Navbar>
@@ -51,7 +59,6 @@ export default function Profile() {
         </>
       )
     } else {
-      console.log(lang, portfolio);
       return (
         <>
           <S_Navbar lang={lang} isLoggedIn={true}></S_Navbar>
