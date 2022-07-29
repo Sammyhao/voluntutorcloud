577855646d3f559b5c920e095260947973e2a021
Ruby Chang <ruby931109@gmail.com>
2022-07-29 17:43:42 +0800

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -119,7 +119,7 @@ function HeroSection() {
           </div>
         ) : (
           <div>
-            <h1 id="homePageTitle">{a[status]}</h1>
+            {/* <h1 id="homePageTitle">{a[status]}</h1>
             <p id="homePageSubTitle">
               {role ? <div>{b[status]}</div> : <div>{e[status]}</div>}
             </p>
@@ -130,7 +130,7 @@ function HeroSection() {
               <Link to="/sign-in" className="btn-mobile">
                 <div className="btn-hero">{d[status]}</div>
               </Link>
-            </div>
+            </div> */}
           </div>
         )}
       </div>
