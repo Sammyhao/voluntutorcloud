1503d796dbb1ff9513cbcd5199e494aaefe52cb4
Ruby Chang <ruby931109@gmail.com>
2022-08-17 11:55:48 +0800

    redux-persist

--- src/index.js
+++ src/index.js
@@ -5,21 +5,36 @@ import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import userReducer from './reducer/user'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import createSagaMiddleware from 'redux-saga'
 import mySaga from './saga/index'
+import { PersistGate } from 'redux-persist/es/integration/react'
+// import storageSession from 'reduxjs-toolkit-persist/lib/storage/session'
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 const saga = createSagaMiddleware()
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
   middleware: [saga],
 })
+
+const Persistor = persistStore(store)
+
 saga.run(mySaga)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={Persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 )
