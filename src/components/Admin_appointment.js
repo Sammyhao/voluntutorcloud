4d56d38f5c7e5b011632b79677a7a985f3c7e603
Ruby Chang <ruby931109@gmail.com>
2023-05-30 21:15:42 +0800

    admin

--- src/components/Admin_appointment.js
+++ src/components/Admin_appointment.js
@@ -5,54 +5,73 @@ import Axios from 'axios'
 
 export default function Admin_appointment() {
 	const [isLoading, setLoading] = useState(true)
-	const [stpair, setStpair] = useState([])
 	const [servHr, setServHr] = useState([]);
-	const [ech, setEch] = useState(4); // manually change the echelon number
+	const [ech, setEch] = useState(5);
+	const [inputech, setinputech] = useState(ech)
 
-	const DisplayServHr = () => {
-		for(let i = 0; i < servHr.length; i++) {
-			console.log(servHr[i]);
-		}
+	const fetchdata = () => {
+		Axios.post(
+			'https://voluntutorcloud-server.herokuapp.com/findAllContact',
+		).then((response) => {
+			for (let i = 0; i < response.data.length; i++) {
+				let tempStpair = response.data[i]
+				Axios.post('https://voluntutorcloud-server.herokuapp.com/getRecord', {
+					username: tempStpair.username,
+					studentname: tempStpair.studentname,
+					studentmail: tempStpair.studentmail,
+					echelon: ech,
+				}).then((response) => {
+					let hr = 0;
+					for (let i = 0; i < response.data.length; i++) {
+						hr += response.data[i].duration;
+						// console.log(response.data[i].duration);
+					}
+					setServHr(servHr => [...servHr, { trUsername: tempStpair.username, stuName: tempStpair.studentname, hr: hr }]);
+				})
+			}
+			setLoading(false);
+		})
 	}
 
 	useEffect(() => {
-			Axios.post(
-				'https://voluntutorcloud-server.herokuapp.com/findAllContact',
-			).then((response) => {
-				for (let i = 0; i < response.data.length; i++) {
-					let tempStpair = response.data[i]
-					setStpair((stpair) => [...stpair, tempStpair])
-					Axios.post('https://voluntutorcloud-server.herokuapp.com/getRecord', {
-						username: tempStpair.username,
-						studentname: tempStpair.studentname,
-						studentmail: tempStpair.studentmail,
-						echelon: ech,
-					}).then((response) => {
-						let hr = 0;
-						for (let i = 0; i < response.data.length; i++) {
-							hr += response.data[i].duration;
-							// console.log(response.data[i].duration);
-						}
-						setServHr(servHr => [...servHr, { trUsername: tempStpair.username, stuName: tempStpair.studentname, hr: hr }]);
-					})
-				}
-				setLoading(false);
-			})
+		fetchdata()
 	}, [ech])
-	if(isLoading) {
+	if (isLoading) {
 		return <Loading />
 	} else {
+		console.log(servHr);
 		return (
 			<>
-				<input
-					type="text"
-					placeholder="Input echelon number"
-					value={ech}
-					onChange={(e) => {setEch(e.target.value);}}
-				></input>
-				<div onClick={DisplayServHr}>Press me to see service hour list</div>
+				<div className='admin_wrap'>
+					<div className='admin_title'>服務時數紀錄（第{ech}梯次）</div>
+					<input
+						className="admin_input"
+						type="number"
+						value={inputech}
+						placeholder="請輸入欲查詢梯次"
+						onChange={(e) => {
+							setinputech(e.target.value)
+						}}
+					/>
+					<button
+						className="admin_button"
+						onClick={() => {
+							setEch(inputech);
+							setinputech("");
+
+							setServHr([])
+						}}>
+						Search
+					</button>
+					<div className='admin_chart_pu'>
+						{servHr.map((e, ind) => {
+							return (
+								<div key={ind}>{e.trUsername}:&nbsp;{e.hr}&nbsp;hr</div>
+							)
+						})}
+					</div>
+				</div>
 			</>
 		)
 	}
-	return <></>
 }
