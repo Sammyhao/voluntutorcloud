85ce9b49e9315f12cb18bf4d085a040fff9098b6
sean12-2007 <xiaoyanghe12@gmail.com>
2023-11-01 20:49:40 +0800

    Kyle

--- src/components/Team.js
+++ src/components/Team.js
@@ -15,16 +15,18 @@ export default function Team() {
   // titles
   let a = ['Founders Team', '創辦團隊 執行總監']
   let b = ['Ruby Chang', '張舒晴 Ruby']
-  let c = ['President', '領導者']
+  let c = ['Founding President', '創辦領導者']
   let d = ['Engineering Director', '開發總監']
   let e = ['Sam Hao', '郝胤翔 Sam']
-  let f = ['Vice President', '副領導者']
+  let f = ['Founding Vice President', '副創辦領導者']
   let g = ['Carol Kao', '高婉予 Carol']
   let h = ['Marketing Director', '行銷總監']
   let i = ['PR Director', '公關領導']
   let j = ['Zachary Lai', '賴元斌 Zachary']
   let k = ['Daniel Yu', '余浩瑋 Daniel']
   let l = ['HR Director', '人資領導']
+  let m = ['Kyle Wang', '王聖元']
+  let n = ['Vice President', '副領導者']
 
   return (
     <div className="about_uscont">
@@ -39,7 +41,7 @@ export default function Team() {
             ></img>
             <div class="image_team_overlay image_team_overlay -- blur">
               <div class="image_text_team">
-                <b className="image_text_head">{b[status]}</b>
+                <b className="image_text_head">{m[status]}</b>
                 <div
                   style={{
                     marginTop: '10px',
@@ -49,7 +51,7 @@ export default function Team() {
                     justifyContent: 'center',
                   }}
                 >
-                  <div className="image_text_roles">{c[status]}</div>
+                  <div className="image_text_roles">{n[status]}</div>
                   <div className="image_text_roles">{d[status]}</div>
                 </div>
               </div>
@@ -73,8 +75,7 @@ export default function Team() {
                     justifyContent: 'center',
                   }}
                 >
-                  <div className="image_text_roles">{c[status]}</div>
-                  <div className="image_text_roles">{d[status]}</div>
+                  <div className="image_text_roles">{n[status]}</div>
                 </div>
               </div>
             </div>
